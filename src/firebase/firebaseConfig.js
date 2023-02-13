e5009ac2db4833f6de47630a37a7e8120b70ba20
Samuel Alves <samueltheodoro21@gmail.com>
2023-02-13 16:54:58 -0300

    git ignore update

--- src/firebase/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB0p-rO05SlSVKxyJUOqUFpFEXzRf8ExdM",
-  authDomain: "diary-app-71be3.firebaseapp.com",
-  projectId: "diary-app-71be3",
-  storageBucket: "diary-app-71be3.appspot.com",
-  messagingSenderId: "17967340276",
-  appId: "1:17967340276:web:b0dc313d5b39c68bf49ca1"
-};
